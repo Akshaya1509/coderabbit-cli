3f55f2ff5b06004d2dee46dae38c5e9a440f479e
coder09dev <akshayamukundhan2@gmail.com>
2024-08-28 14:05:45 +0530

    Update GithubAPI name and getTopNLanguages

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 // import yargs from 'yargs';
 import { getUsername } from "../src/utils/utils.js";
-import { Github } from "../src/lib/Github.js";
+import { GithubAPI } from "../src/lib/Github.js";
 // import { Gitlab } from "../src/lib/Gitlab.js";
-import { TopNLanguages } from '../src/lib/TopNLanguages.js';
+
 
 async function main() {
     try {
@@ -11,17 +11,10 @@ async function main() {
         const profileUrl = 'https://github.com/NatoBoram';
         const username = getUsername(profileUrl);
 
-        const githubTopN = Github.getTopNLanguages(username);
+        const githubTopN = await GithubAPI.getTopNLanguages(username);
+        console.log(githubTopN);
         // const gitLabTopN = Gitlab.getTopNLanguages(username);
-        let results = Promise.all(
-            githubTopN, 
-            gitLabTopN
-        );
         
-        // return TopNLanguages.mergeLists(
-        //     githubTopN,
-        //     gitLabTopN,
-        // );
     } catch(err) {
         console.log(err);
     }
