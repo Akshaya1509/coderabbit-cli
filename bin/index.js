9a90de79c275708c0d16ef2e616cc10dd6d12811
coder09dev <akshayamukundhan2@gmail.com>
2024-08-28 19:40:50 +0530

    Add profile url as command line param

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
-// import yargs from 'yargs';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import { getUsername } from "../src/utils/utils.js";
 import { GithubAPI } from "../src/lib/GithubAPI.js";
 import { printLanguages } from "../src/utils/utils.js";
 
 async function main() {
     try {
-        // TODO: get profile url as cli input
-        const profileUrl = 'https://github.com/NatoBoram';
+        const argv = yargs(hideBin(process.argv))
+            .option('profile-url', {
+                alias: 'url',
+                describe: 'Github profile URL',
+                type: 'string',
+                demandOption: true
+            })
+            .check((argv) => {
+                if (!argv.profile_url) throw new Error("Profile URL must be passed");
+                return true; 
+            })
+            .argv;
+
+        const profileUrl = argv.profile_url;
         const username = getUsername(profileUrl);
 
         const githubTopN = await GithubAPI.getTopNLanguages(username);
