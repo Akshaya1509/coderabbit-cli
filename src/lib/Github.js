ecd901ce3834e0e3533d3e1b0385b4f01a14a946
coder09dev <akshayamukundhan2@gmail.com>
2024-08-28 13:56:46 +0530

    Update getTopNLanguages to use promises for API calls

--- src/lib/Github.js
+++ src/lib/Github.js
@@ -1,23 +1,29 @@
 import { getAllRepos, getAllLanguages } from "../utils/github_utils.js";
 import { getLanguagesCount, getTopN } from "../utils/utils.js";
 
-const Github = (function () {
-    const getTopNLanguages = async (username) => {
-        const repos = await getAllRepos(username);
-        const languagesMap = new Map();
+const GithubAPI = (function () {
+    const getTopNLanguages = async function (username) {
+        const reposData = await getAllRepos(username);    
+        const repos = reposData.map(repository => repository.name);
         
-        // create promise for getting langs from each repo - make it all async 
-        repos.forEach(async repo => {
-            const languages = await getAllLanguages(username, repo);
-            // console.log(languages);
-            getLanguagesCount(languagesMap, languages);
+        const promises = []; 
+        repos.forEach(repositoryName => {
+            promises.push(getAllLanguages(username, repositoryName));
         });
-
+    
+        const results = await Promise.allSettled(promises);
+        const languages = results
+            .filter(result => result.status === 'fulfilled')
+            .map(result => result.value);
+        
+        const languagesMap = new Map();
+        getLanguagesCount(languagesMap, languages);
+    
         const topNLanguages = getTopN(languagesMap);
         return topNLanguages;
-    }
+    }    
 
-    return getTopNLanguages;
+    return { getTopNLanguages };
 })();
 
-export { Github };
+export { GithubAPI };
