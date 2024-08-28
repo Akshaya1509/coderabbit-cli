0a9f27d57f47bd2c82373fc46c2f8671ec0a0e28
coder09dev <akshayamukundhan2@gmail.com>
2024-08-28 13:58:55 +0530

    Use AUTH_TOKEN for octokit API calls and updated argument name of getAllLanguages

--- src/utils/github_utils.js
+++ src/utils/github_utils.js
@@ -1,6 +1,9 @@
 import { Octokit } from "octokit";
+import "dotenv/config";
 
-const octokit = new Octokit();
+const octokit = new Octokit({
+    auth: process.env.GH_AUTH_TOKEN
+});
 
 async function getAllRepos(username) {
     const response = await octokit.rest.repos.listForUser({
@@ -10,10 +13,10 @@ async function getAllRepos(username) {
     return response.data;
 }
 
-async function getAllLanguages(username, repo) {
+async function getAllLanguages(username, repositoryName) {
     const response = await octokit.rest.repos.listLanguages({
         owner: username,
-        repo
+        repo: repositoryName
     });
     return response.data;
 }
